39d9bf40108f0d9c3bf38838978bd5655b533a6b
leeks123 <kensei719@naver.com>
2024-02-03 15:30:33 +0900

    feat: setup font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_KR, Noto_Sans, Lobster } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const lobster = Lobster({ weight: "400", subsets: ["latin"] });
+const notoKR = Noto_Sans_KR({ subsets: ["latin"] });
+const noto = Noto_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body className={cn(lobster.className, notoKR.className, noto.className)}>
+        {children}
+      </body>
     </html>
   );
 }
